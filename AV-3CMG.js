a3059c93198b5b1e194ba4f9f5e30276f4c947a0
@Car-png <cardenasjavier1846@gmail.com>
2020-05-31 04:35:45 -0500

    The Last

--- AV-3CMG.js
+++ AV-3CMG.js
@@ -6,12 +6,43 @@ if(annyang){
             window.open('https://www.google.com/search?sxsrf=ALeKk02R3u6nWG1-FvkdLQFKMlHZOGg36Q%3A1590901089781&source=hp&ei=YTnTXsO-LbvQ5OUPibiA6Ao&q='+valor+'','_blank');
             
         },
-        'texto': function(){
-            window.open('VozText.html','_blank');
-        },
-        'Cerrar':function(){
+        'cerrar':function(){
             window.close();
+        },
+
+        'apagarpc': Apagar(),
+        'reiniciarpc': Restart(),
+        'restartpc': Restart(),
+        'cancelar': Cancelar(),
+
+        'youtube': function(){
+            window.open('https://www.youtube.com/','_blank');
+        },
+        'face': function(){
+            window.open('https://www.facebook.com/','blank_');
+        },
+        'facebook': function(){
+            window.open('https://www.facebook.com/','blank_');
+        },
+        'messenger': function(){
+            window.open('https://www.messenger.com/','blank_');
+        },
+        'twitter': function(){
+            window.open('https://twitter.com/explore','blank_');
+        },
+        'gmail': function(){
+            window.open('https://mail.google.com/','blanc_');
+        },
+        'twitch': function(){
+            window.open('https://www.twitch.tv/','blank_');
+        },
+        'instagram': function(){
+            window.open('https://www.instagram.com/','blank_');
+        },
+        'insta': function(){
+            window.open('https://www.instagram.com/','blank_');
         }
+
     };
 
             //Añadir    commands    a annyang
@@ -22,4 +53,20 @@ if(annyang){
 
     //Empezar a escuchar
     annyang.start({continuous:false});
+}
+
+
+function Apagar(){
+    let a = document.getElementById('batA').open;
+    return a;
+}
+
+function Restart(){
+    let r = document.getElementById('batR').open;
+    return r;
+}
+
+function Cancelar(){
+    let c = document.getElementById('batC').open;
+    return c;
 }
